52ab779c16bb0a79a3a1e05d4a75446276c8cf65
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-03-19 10:34:12 -0500

    dropping/hovering getting better

--- js/app.js
+++ js/app.js
@@ -12,7 +12,9 @@ const game = {
 
  	
 
-
+		newGame: function (){
+			//game.pla
+		}
 	
  	
 		
@@ -29,7 +31,10 @@ const game = {
 	
 	
  	
-		
+	document.addEventListener('click', () => {
+		if(event.targetr && event.target.id === "newGame")
+			game.newGame();
+	})	
 
 
 	document.addEventListener('dragstart', (event) => {
@@ -52,34 +57,36 @@ const game = {
 
 
 	document.addEventListener('dragover', (event) => { 
-		if(event.target && event.target.className === 'imageContainer'){
+		if(event.target && event.target.classList.contains('imageContainer')) {
 			event.preventDefault()
-			game.currentSquare = event.target
+			//game.currentSquare = event.target
 			
 		}
 	})
 		
 
 	
-	document.addEventListener('dragenter' ,(event) => {
-		if(event.target && event.target.className === 'imageContainer'){
+	document.addEventListener('dragenter' ,(event) => {		
+		if(event.target && event.target.classList.contains('imageContainer')){
+			console.log("inside an image container");
 			event.preventDefault();
 			game.currentSquare = event.target
-			game.currentSquare.className += ' hovered'
-			console.log(game.currentSquare.className)			
+			game.currentSquare.classList.add('hovered');
+			console.log("game.currentSquare in dragEnter, should contain hovered", game.currentSquare)			
 		}
 	})
 	
 	document.addEventListener('dragleave',(event) => {
-		if(event.target && event.target.className === 'imageContainer hovered'){
-			game.currentSquare.className = 'imageContainer' 
-			console.log(game.currentSquare.className)
+		if(event.target && event.target.classList.contains('hovered')){
+			game.currentSquare.classList.remove('hovered')
+			console.log("in dragleave, hovered should be removed", game.currentSquare)
 			
 		}
 	})
 			
 	document.addEventListener('drop', (event) => {
-		if(event.target && event.target.className === 'imageContainer'){
+		console.log("here is event.target in drop", event.target);
+		if(event.target && event.target.classList.contains('imageContainer')){
 			console.log(game.currentSquare)
 			game.currentSquare.appendChild(game.currentWhitePawn)
 		}
